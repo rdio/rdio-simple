1de44c98d01b71ef3a0168049cb7264bbb6c5766
R. Kevin Nelson <rkn@rd.io>
2014-01-02 14:19:37 -0800

    Update the web-based example to use hapijs.

--- node/examples/web-based.js
+++ node/examples/web-based.js
@@ -1,136 +1,140 @@
-var link = require("link"),
-    Rdio = require("./../rdio"),
-    cred = require("./rdio_consumer_credentials");
+var Hapi = require('hapi'),
+    Rdio = require('./../rdio'),
+    RdioCredentials = require('./rdio_consumer_credentials');
 
-var app = new link.Builder;
+var options = {
+  maxCookieSize: 0,
+  cookieOptions: {
+    password: 'some secure password',
+    isSecure: false
+  }
+};
 
-app.use(link.commonLogger);
-app.use(link.contentType);
-app.use(link.contentLength);
-app.use(link.sessionCookie);
+var server = new Hapi.Server(8080);
+server.pack.allow({ ext: true }).require('yar', options, function (err) {
+  if (err) {
+    console.log(err);
+    throw err;
+  }
+});
 
-app.route("/", function (env, callback) {
-    var session = env["link.session"];
-    var accessToken = session.at;
-    var accessTokenSecret = session.ats;
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function (request) {
+    var accessToken = request.session.get('accessToken');
+    if (accessToken) {
+      var rdio = new Rdio([RdioCredentials.RDIO_CONSUMER_KEY, RdioCredentials.RDIO_CONSUMER_SECRET],
+                          [accessToken.token, accessToken.secret]);
+      rdio.call('currentUser', function(err, data) {
+        if (err) {
+          console.log(err);
+          request.reply(new Error("Error getting current user"));
+        }
 
-    if (accessToken && accessTokenSecret) {
-        var rdio = new Rdio([cred.RDIO_CONSUMER_KEY, cred.RDIO_CONSUMER_SECRET],
-                            [accessToken, accessTokenSecret]);
+        var currentUser = data.result;
 
-        rdio.call("currentUser", function (err, data) {
-            if (err && link.handleError(err, env, callback)) {
-                return;
-            }
+        rdio.call('getPlaylists', function(err, data) {
+          if (err) {
+            console.log(err);
+            request.reply(new Error("Error getting playlists"));
+          }
 
-            var currentUser = data.result;
+          var playlists = data.result.owned;
 
-            rdio.call("getPlaylists", function (err, data) {
-                if (err && link.handleError(err, env, callback)) {
-                    return;
-                }
+          var body = [];
+          body.push('<html><head><title>Rdio-Simple Example</title></head><body>');
+          body.push('<p>' + currentUser.firstName + "'s playlists:</p>");
+          body.push('<ul>');
 
-                var playlists = data.result.owned;
+          playlists.forEach(function(playlist) {
+            body.push('<li><a href="' + playlist.shortUrl + '">' + playlist.name + '</a></li>');
+          });
+          body.push('</ul>');
+          body.push('<a href="/logout">Log out of Rdio</a></body></html>');
 
-                var body = [];
-
-                body.push("<html><head><title>Rdio-Simple Example</title></head><body>");
-                body.push("<p>" + currentUser.firstName + "'s playlists:</p>");
-                body.push("<ul>");
-
-                playlists.forEach(function (playlist) {
-                    body.push('<li><a href="' + playlist.shortUrl + '">' + playlist.name + '</a></li>');
-                });
-
-                body.push("</ul>");
-                body.push('<a href="/logout">Log out of Rdio</a></body></html>');
-
-                callback(200, {}, body.join("\n"));
-            });
+          request.reply(body.join('\n'));
         });
+      });
     } else {
-        var body = [];
+      var body = [];
 
-        body.push('<html><head><title>Rdio-Simple Example</title></head><body>');
-        body.push('<a href="/login">Log into Rdio</a>');
-        body.push('</body></html>');
+      body.push('<html><head><title>Rdio-Simple Example</title></head><body>');
+      body.push('<a href="/login">Log into Rdio</a>');
+      body.push('</body></html>');
 
-        callback(200, {}, body.join("\n"));
+      request.reply(body.join('\n'));
     }
+  }
 });
 
-app.route("/login", function (env, callback) {
-    var session = env["link.session"] = {};
-    var req = new link.Request(env);
+server.route({
+  method: 'GET',
+  path: '/login',
+  handler: function(request) {
+    // begin the authentication process
+    var rdio = new Rdio([RdioCredentials.RDIO_CONSUMER_KEY, RdioCredentials.RDIO_CONSUMER_SECRET]);
+    var callbackUrl = request.server.info.protocol + "://" + request.info.host + "/callback";
+    rdio.beginAuthentication(callbackUrl, function(err, authUrl) {
+      if (err) {
+        console.log(err);
+        request.reply(new Error("Error beginning authentication"));
+      }
 
-    // Begin the authentication process.
-    var rdio = new Rdio([cred.RDIO_CONSUMER_KEY, cred.RDIO_CONSUMER_SECRET]);
-    var callbackUrl = req.baseUrl + "/callback";
+      request.session.set('requestToken', {
+        token: rdio.token[0],
+        secret: rdio.token[1]
+      });
 
-    rdio.beginAuthentication(callbackUrl, function (err, authUrl) {
-        if (err && link.handleError(err, env, callback)) {
-            return;
-        }
-
-        // Save the request token/secret in the session.
-        session.rt = rdio.token[0];
-        session.rts = rdio.token[1];
-
-        // Go to Rdio to authenticate the app.
-        redirect(authUrl, callback);
+      request.reply.redirect(authUrl);
     });
-}, "GET");
-
-app.route("/callback", function (env, callback) {
-    var session = env["link.session"];
-    var req = new link.Request(env);
-
-    req.params(function (err, params) {
-        if (err && link.handleError(err, env, callback)) {
-            return;
-        }
-
-        var requestToken = session.rt;
-        var requestTokenSecret = session.rts;
-        var verifier = params.oauth_verifier;
-
-        if (requestToken && requestTokenSecret && verifier) {
-            // Exchange the verifier and token for an access token.
-            var rdio = new Rdio([cred.RDIO_CONSUMER_KEY, cred.RDIO_CONSUMER_SECRET],
-                                [requestToken, requestTokenSecret]);
-
-            rdio.completeAuthentication(verifier, function (err) {
-                if (err && link.handleError(err, env, callback)) {
-                    return;
-                }
-
-                // Save the access token/secret in the session (and discard the
-                // request token/secret).
-                session.at = rdio.token[0];
-                session.ats = rdio.token[1];
-                delete session.rt;
-                delete session.rts;
-
-                // Go to the home page.
-                redirect("/", callback);
-            });
-        } else {
-            // We're missing something important.
-            redirect("/logout", callback);
-        }
-    });
-}, "GET");
-
-app.route("/logout", function (env, callback) {
-    env["link.session"] = {};
-    redirect("/", callback);
-}, "GET");
-
-var server = link.run(app, {}, function () {
-    var addr = server.address();
-    console.log("Link server started at %s:%s", addr.address, addr.port);
+  }
 });
 
-function redirect(location, callback) {
-    callback(302, {"Location": location}, 'Go to <a href="' + location + '">' + location + '</a>');
-}
+
+server.route({
+  method: 'GET',
+  path: '/callback',
+  handler: function(request) {
+    var requestToken = request.session.get('requestToken');
+    var verifier = request.query.oauth_verifier;
+
+    if (requestToken && verifier) {
+      // exchange the request token and verifier for an access token.
+      var rdio = new Rdio([RdioCredentials.RDIO_CONSUMER_KEY, RdioCredentials.RDIO_CONSUMER_SECRET],
+                          [requestToken.token, requestToken.secret]);
+      rdio.completeAuthentication(verifier, function(err) {
+        if (err) {
+          console.log(err);
+          request.reply(new Error("Error completing Authentication"));
+        }
+
+        request.session.set('accessToken', {
+          token: rdio.token[0],
+          secret: rdio.token[1]
+        });
+        request.session.clear('requestToken');
+
+        request.reply.redirect('/');
+      });
+    } else {
+      request.reply.redirect('/logout');
+    }
+  }
+});
+
+
+server.route({
+  method: 'GET',
+  path: '/logout',
+  handler: function(request) {
+    request.session.reset();
+    request.reply.redirect('/');
+  }
+});
+
+
+
+server.start(function() {
+  console.log('Server started on port:', server.info.port);
+});
