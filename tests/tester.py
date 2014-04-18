2cf1e304a2086191af3328264768c3049dc62d6c
Devin Sevilla <devin.sevilla@rd.io>
2014-04-18 10:40:52 -0700

    Python 3 support for tests

--- tests/tester.py
+++ tests/tester.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python
 
-import sys,os.path, urlparse, sys
+import sys, os.path
+
+try:
+    from urllib.parse import parse_qsl
+except ImportError:
+    from urlparse import parse_qsl
+
+PY3 = (sys.version_info >= (3, 0, 0))
+
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'python'))
 from om import om
 
 consumer = (sys.argv[1], sys.argv[2])
 url = sys.argv[3]
-params = [(k.decode('UTF-8'),v.decode('UTF-8')) for k, v in urlparse.parse_qsl(sys.argv[4])]
+
+if PY3:
+    params = [(k, v) for k, v in parse_qsl(sys.argv[4])]
+else:
+    params = [(k.decode('utf-8'), v.decode('utf-8')) for k, v in parse_qsl(sys.argv[4])]
+
 if sys.argv[5] != '' and sys.argv[6] != '':
   token = (sys.argv[5], sys.argv[6])
 else:
